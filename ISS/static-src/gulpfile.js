a797577f8e8009606a394e97f8ea6c998ee4a895
RyanJenkins <rjenkins@mail.sfsu.edu>
2016-10-07 14:11:38 -0700

    Set up less compilation

---
+++ ISS/static-src/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var flatten = require('gulp-flatten');
+var less = require('gulp-sources-less');
+var path = require('path');
+ 
+gulp.task('less', function() {
+  return gulp.src('./src/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(flatten())
+    .pipe(gulp.dest('../static/css'));
+});
+
+gulp.task('generate', ['less']);
+
+gulp.task('watch', ['generate'], function() {
+  gulp.watch([ './src/**/less' ], ['less']);
+});
