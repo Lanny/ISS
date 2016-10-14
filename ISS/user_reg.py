ab1504640eebf5a85dbf5f4b09779af5cd410795
NullArray <opsec@null.net>
2016-10-14 11:07:57 +0200

    Update user_reg.py

--- ISS/user_reg.py
+++ ISS/user_reg.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect    
 from django.contrib import auth                 
 from django.core.context_processors import csrf 
-from forms import MyRegistrationForm
+from rforms import MyRegistrationForm
 
 def register_user(request):
     if request.method == 'POST':
