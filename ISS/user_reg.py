23d9b644a4dfaeb34663b556ca5c6de5be4bfe44
NullArray <opsec@null.net>
2016-10-14 11:06:50 +0200

    Create user_reg.py

---
+++ ISS/user_reg.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect    
+from django.contrib import auth                 
+from django.core.context_processors import csrf 
+from forms import MyRegistrationForm
+
+def register_user(request):
+    if request.method == 'POST':
+        form = MyRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/accounts/register_success')
+    args = {}
+    args.update(csrf(request))
+    args['form'] = MyRegistrationForm()
+    print args
+    return render(request, 'register.html', args)
