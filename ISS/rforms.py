531e37c37912926cd36f8ff96a094308f53e8164
NullArray <opsec@null.net>
2016-10-16 03:22:18 +0200

    Update rforms.py

--- ISS/rforms.py
+++ ISS/rforms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User   
 from django.contrib.auth.forms import UserCreationForm      
 
-class MyRegistrationForm(UserCreationForm):
+class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required = True)
     first_name = forms.CharField(required = False)
     last_name = forms.CharField(required = False)
