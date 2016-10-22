c327e8b15a86b431bb8c97abf50d0209b1ec03b8
RyanJenkins <rjenkins@mail.sfsu.edu>
2016-10-22 15:24:57 -0700

    Unbroke sourcemaps I guess.

--- ISS/static-src/gulpfile.js
+++ ISS/static-src/gulpfile.js
@@ -1,18 +1,22 @@
 var gulp = require('gulp');
 var flatten = require('gulp-flatten');
 var less = require('gulp-sources-less');
+var sourcemaps = require('gulp-sourcemaps');
 var path = require('path');
+
  
 gulp.task('less', function() {
   var lessStream = less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+      paths: [ path.join(__dirname, 'less') ]
     }).on('error', function(err) {
       console.error(err);
       this.emit('end');
     });
 
   return gulp.src('./src/**/*.less')
+    .pipe(sourcemaps.init())
     .pipe(lessStream)
+    .pipe(sourcemaps.write())
     .pipe(flatten())
     .pipe(gulp.dest('../static/css'));
 });
