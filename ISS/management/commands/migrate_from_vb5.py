0eda53ace368463266fed2fb90ee04fc7ffe2493
RyanJenkins <rjenkins@mail.sfsu.edu>
2016-10-30 21:32:29 -0700

    Thread migration seems to work, hard to tell without posts.

--- ISS/management/commands/migrate_from_vb5.py
+++ ISS/management/commands/migrate_from_vb5.py
@@ -6,6 +6,8 @@ from django.db.utils import IntegrityError
 from django.core.management.base import BaseCommand, CommandError
 from ISS.models import *
 
+migration_version = 'DEVELOPMENT_001'
+
 class Command(BaseCommand):
     help = 'Migrates a vBulletin 5 database to ISS'
 
@@ -62,6 +64,31 @@ class Command(BaseCommand):
 
         return o2n_map
 
+    def mig_threads(self, cnx, cursor, forum_pk_map):
+        threads = cursor.execute("""
+            SELECT thread.* FROM node AS thread
+                JOIN node AS forum
+                    ON thread.parentid=forum.nodeid
+                JOIN node AS forum_group
+                    ON forum.parentid=forum_group.nodeid
+                JOIN node AS forum_parent
+                    ON forum_parent.nodeid=forum_group.parentid 
+                WHERE 
+                    forum_parent.urlident = 'forum'
+        """)
+
+        o2n_map = {}
+        for thread in cursor:
+            new_thread = Thread(
+                title=thread['title'],
+                created=thread['publishdate'],
+                forum_id=forum_pk_map[thread['parentid']],
+                log='Migrated at %s' % migration_version)
+
+            new_thread.save()
+
+            o2n_map[thread['nodeid']] = new_thread.pk
+
 
     def handle(self, *args, **kwargs):
 
@@ -71,8 +98,9 @@ class Command(BaseCommand):
                                       database=self.db_name)
         cursor = cnx.cursor(dictionary=True)
 
-        #user_pk_map = self.mig_users(cnx, cursor)
-        self.mig_forums(cnx, cursor)
+        user_pk_map = self.mig_users(cnx, cursor)
+        forum_pk_map = self.mig_forums(cnx, cursor)
+        thread_pk_map = self.mig_threads(cnx, cursor, forum_pk_map)
         
         cnx.close()
 
