0239827cd47a73afac4f3ce4621a68a4bd433462
RyanJenkins <rjenkins@mail.sfsu.edu>
2016-11-13 12:46:28 -0800

    Added custom user title field which I guess I missed adding a mig for prior.

---
+++ ISS/migrations/0010_auto_20161113_2046.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ISS', '0009_auto_20161108_2103'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='poster',
+            name='custom_user_title',
+            field=models.CharField(default=None, max_length=256, null=True, blank=True),
+        ),
+    ]
