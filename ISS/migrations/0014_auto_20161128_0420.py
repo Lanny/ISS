8d4b8c1d1b34bc21ccd2e70e34c8d2deee64ce61
RyanJenkins <rjenkins@mail.sfsu.edu>
2016-11-27 20:34:27 -0800

    Added post body serach index migration.

---
+++ ISS/migrations/0014_auto_20161128_0420.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ISS', '0013_auto_20161119_2100'),
+    ]
+
+    _forwards_sql = """
+        CREATE INDEX post_contents_ft_index ON "ISS_post"
+        USING GIN(to_tsvector('english', content));
+    """
+    _backwards_sql = 'DROP INDEX post_contents_ft_index;'
+
+    operations = [
+        migrations.RunSQL(_forwards_sql, _backwards_sql)
+    ]
