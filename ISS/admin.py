475bd8461959bbb5fa97af4e4b46d68e33b4d62f
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-02-19 00:28:33 -0800

    Remove avatar from admin interface for Posters

--- ISS/admin.py
+++ ISS/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Poster)
 admin.site.register(Forum)
 admin.site.register(Thread)
 admin.site.register(Post)
 admin.site.register(Thanks)
+
+@admin.register(Poster)
+class PosterAdmin(admin.ModelAdmin):
+    exclude = ('avatar',)
 
 @admin.register(PrivateMessage)
 class PrivateMessageAdmin(admin.ModelAdmin):
