6e09d538432cdf5b6d7c2a34d52633719bde43c7
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-02-19 00:36:51 -0800

    Better post admin.

--- ISS/admin.py
+++ ISS/admin.py
@@ -4,13 +4,23 @@ from .models import *
 
 admin.site.register(Forum)
 admin.site.register(Thread)
-admin.site.register(Post)
 admin.site.register(Thanks)
 
 @admin.register(Poster)
 class PosterAdmin(admin.ModelAdmin):
     exclude = ('avatar',)
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'thread_name', 'short_content')
+
+    def thread_name(self, obj):
+        return obj.thread.title[:100]
+
+    def short_content(self, obj):
+        return obj.content[:200]
+
 @admin.register(PrivateMessage)
 class PrivateMessageAdmin(admin.ModelAdmin):
     list_display = ('subject', 'sender', 'receiver', 'created')
+
