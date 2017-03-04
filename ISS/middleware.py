232e54776b240ec1c3fa5051e4d40b714ec6d2d8
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-03-03 21:11:03 -0800

    Added middleware for reporting unhandled exceptions.

--- ISS/middleware.py
+++ ISS/middleware.py
@@ -1,6 +1,10 @@
 import pytz
+import traceback
 
+from django.conf import settings
 from django.utils import timezone
+
+from ISS.models import *
 
 class TimezoneMiddleware(object):
     def process_request(self, request):
@@ -8,5 +12,29 @@ class TimezoneMiddleware(object):
             timezone.activate(pytz.timezone(request.user.timezone))
         else:
             timezone.activate('UTC')
+
+        return None
+
+class PMAdminMiddleware(object):
+    def process_exception(self, request, exception):
+        message = '''
+            Encountered exception when processing request.
+            Request Path: %s
+            Request Method: %s
+            Active User: %s
+
+            Stack Trace:
+            [code]%s[/code]
+        ''' % (
+                request.path,
+                request.method,
+                request.user,
+                traceback.format_exc(exception))
+
+        PrivateMessage.send_pm(
+            Poster.get_or_create_system_user(),
+            Poster.objects.filter(is_admin=True),
+            'Error encountered in processing request',
+            message)
 
         return None
--- test_settings.py
+++ test_settings.py
@@ -49,7 +49,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'ISS.middleware.TimezoneMiddleware'
+    'ISS.middleware.TimezoneMiddleware',
+    'ISS.middleware.PMAdminMiddleware'
 )
 
 ROOT_URLCONF = 'ISS.urls'
