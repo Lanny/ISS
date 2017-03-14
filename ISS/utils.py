b70402f3faa4daee3ee601f1259783872747c7aa
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-03-13 20:28:56 -0700

    More graceful failure when unauthenticated user attempts to take action that requires unbanned state.

--- ISS/utils.py
+++ ISS/utils.py
@@ -75,7 +75,7 @@ class MethodSplitView(object):
                 return HttpResponseForbidden('You must be staff to do this.')
 
         if getattr(self, 'unbanned_required', False):
-            if request.user.is_banned():
+            if not request.user.is_authenticated() or request.user.is_banned():
                 return get_ban_403_response(request)
 
         meth = getattr(self, request.method, None)
