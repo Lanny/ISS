2b0997b97bc572b0ec645cbc7bc6c74755e3e34c
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-05-21 19:48:54 -0700

    Added tests for thread by user and user profile views.

--- ISS/tests/general_view_tests.py
+++ ISS/tests/general_view_tests.py
@@ -2,27 +2,18 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from ISS.models import *
 
+import test_utils
+
 class GeneralViewTestCase(TestCase):
     def setUp(self):
-        self.scrub = Poster(username='scrub')
+        test_utils.create_std_forums()
+
+        self.scrub = test_utils.create_user(thread_count=5, post_count=10)
         self.scrub.save()
 
         self.scrub_client = Client()
         self.scrub_client.force_login(self.scrub)
 
-        self.general_cat = Category(name='general')
-        self.general_cat.save()
-
-        self.trash_cat = Category(name='trash cat')
-        self.trash_cat.save()
-
-        self.forum = Forum(name='test forum', category=self.general_cat)
-        self.forum.save()
-
-        self.trash_forum = Forum(name='trash forum',
-                                 is_trash=True,
-                                 category=self.trash_cat)
-        self.trash_forum.save()
 
     def test_authed_users_can_access_index(self):
         path = reverse('forum-index')
@@ -40,3 +31,15 @@ class GeneralViewTestCase(TestCase):
         response = self.scrub_client.get(path)
         self.assertEqual(len(response.context['categories']), 2)
         self.assertTrue(isinstance(response.context['forums_by_category'], dict))
+
+    def test_threads_by_user(self):
+        path = reverse('threads-by-user', kwargs={'user_id': self.scrub.pk})
+        response = self.scrub_client.get(path)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['threads']), 5)
+
+    def test_user_profile_doesnt_error(self):
+        path = reverse('user-profile', kwargs={'user_id': self.scrub.pk})
+        response = self.scrub_client.get(path)
+        self.assertEqual(response.status_code, 200)
+
---
+++ ISS/tests/test_utils.py
@@ -0,0 +1,56 @@
+import random
+
+from ISS.models import *
+
+def create_std_forums():
+    general_cat = Category(name='general')
+    general_cat.save()
+
+    trash_cat = Category(name='trash cat')
+    trash_cat.save()
+
+    forum = Forum(name='test forum', category=general_cat)
+    forum.save()
+
+    trash_forum = Forum(name='trash forum',
+                             is_trash=True,
+                             category=trash_cat)
+    trash_forum.save()
+
+USERS_CREATED = 0
+THREADS_CREATED = 0
+
+def create_user(thread_count=0, post_count=0): 
+    global USERS_CREATED
+    global THREADS_CREATED
+
+    USERS_CREATED += 1
+    user = Poster(username='TEST_USER-%d' % USERS_CREATED)
+    user.save()
+
+    for _ in range(thread_count):
+        THREADS_CREATED += 1
+        destination_forum = random.choice(Forum.objects.all())
+
+        thread = Thread(
+            title='TEST_THREAD-%d' % THREADS_CREATED,
+            forum=destination_forum,
+            author=user)
+        thread.save()
+
+        op = Post(
+            author=user,
+            thread=thread,
+            content='opsum ipsum',
+            posted_from='8.8.8.4')
+        op.save()
+
+    for _ in range(post_count):
+        post = Post(
+            author=user,
+            thread=random.choice(Thread.objects.all()),
+            content='postum ipsum',
+            posted_from='8.8.8.8')
+        post.save()
+
+    return user
