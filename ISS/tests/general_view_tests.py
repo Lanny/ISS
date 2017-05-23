dec3c4cd58f926fc0c7d1fe2e4610ca239e27b03
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-05-22 23:33:18 -0700

    Added test cases for post deletion via thread action.

--- ISS/tests/general_view_tests.py
+++ ISS/tests/general_view_tests.py
@@ -9,7 +9,6 @@ class GeneralViewTestCase(TestCase):
         test_utils.create_std_forums()
 
         self.scrub = test_utils.create_user(thread_count=5, post_count=10)
-        self.scrub.save()
 
         self.scrub_client = Client()
         self.scrub_client.force_login(self.scrub)
@@ -42,4 +41,41 @@ class GeneralViewTestCase(TestCase):
         path = reverse('user-profile', kwargs={'user_id': self.scrub.pk})
         response = self.scrub_client.get(path)
         self.assertEqual(response.status_code, 200)
+
+class ThreadActionTestCase(TestCase):
+    def setUp(self):
+        test_utils.create_std_forums()
+
+        self.admin = test_utils.create_user()
+        self.scrub = test_utils.create_user(thread_count=1, post_count=10)
+
+        self.admin.is_admin = True
+        self.admin.is_staff = True
+        self.admin.save()
+
+        self.thread = Thread.objects.all()[0]
+
+        self.scrub_client = Client()
+        self.scrub_client.force_login(self.scrub)
+        self.admin_client = Client()
+        self.admin_client.force_login(self.admin)
+
+    def test_non_staff_may_not_delete_posts(self):
+        path = reverse('thread-action', kwargs={'thread_id': self.thread.pk})
+        response = self.scrub_client.post(path, {'action': 'delete-posts'})
+        self.assertEqual(response.status_code, 403)
+
+    def test_staff_may_delete_posts(self):
+        path = reverse('thread-action', kwargs={'thread_id': self.thread.pk})
+        posts_to_delete = self.thread.post_set.order_by('-created')[8:]
+
+        response = self.admin_client.post(path, {
+            'action': 'delete-posts',
+            'post': [p.pk for p in posts_to_delete]
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.thread.post_set.count(), 8)
+
+
 
