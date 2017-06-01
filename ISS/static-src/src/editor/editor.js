47baf08d4922467149c3a4bf91963bc21bb4b5d1
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-05-31 21:03:25 -0700

    Add hotkeys for bold, underline, italics.

--- ISS/static-src/src/editor/editor.js
+++ ISS/static-src/src/editor/editor.js
@@ -1,5 +1,44 @@
 ;(function() {
   function wrap($) {
+    var wrapOperations = [
+      {
+        name: 'bold',
+        pre: '[b]',
+        post: '[/b]',
+        hotKeyCode: 66
+      },
+      {
+        name: 'underline',
+        pre: '[u]',
+        post: '[/u]',
+        hotKeyCode: 85
+      },
+      {
+        name: 'oblique',
+        pre: '[i]',
+        post: '[/i]',
+        hotKeyCode: 73
+      }
+    ];
+
+    var promptDefaults = {
+      promptText: 'Enter text:'
+    };
+
+    function promptForInput(opts) {
+      var options = $.extend({}, promptDefaults, opts),
+        deferred = $.Deferred(),
+        result = window.prompt(options.promptText);
+
+      if (typeof result === 'string') {
+        deferred.resolve(result);
+      } else {
+        deferred.reject();
+      }
+
+      return deferred.promise();
+    }
+
     function Editor(element) {
       this._el = $(element);
       this._ta = this._el.find('textarea');
@@ -16,8 +55,45 @@
           if (e.keyCode === 13 && (e.ctrlKey || e.metaKey)) {
             e.preventDefault();
             self._el.trigger('submit');
+          } else if (e.ctrlKey || e.metaKey) {
+            for (var i=0; i<wrapOperations.length; i++) {
+              if (e.keyCode === wrapOperations[i].hotKeyCode) {
+                self.executeWrapIntension(wrapOperations[i]);
+                break;
+              }
+            }
           }
         });
+      },
+      executeWrapIntension: function(wrapOp) {
+        var self = this,
+          selStart = self._ta[0].selectionStart,
+          selEnd = self._ta[0].selectionEnd,
+          content = self._ta.val(),
+          preContent = content.substring(0, selStart),
+          selContent = content.substring(selStart, selEnd),
+          postContent = content.substring(selEnd);
+
+        function wrapAndInsertContent(insertionContent) {
+          var wrappedContent = wrapOp.pre + insertionContent + wrapOp.post;
+          self._ta.val(preContent + wrappedContent + postContent);
+
+          // Timeout hack so val change registers and we can set a selection
+          // range
+          setTimeout(function() {
+            self._ta[0].selectionStart = selStart;
+            self._ta[0].selectionEnd = selStart + wrappedContent.length;
+          }, 0);
+        }
+
+        if (selStart === selEnd) {
+          // Nothing selectied, prompt for content
+          promptForInput().done(function(insertionContent) {
+            wrapAndInsertContent(insertionContent);
+          });
+        } else {
+          wrapAndInsertContent(selContent);
+        }
       },
       addQuote: function(quoteCode) {
         var self = this,
@@ -40,3 +116,4 @@
 
   define(['jquery'], wrap);
 })();
+
