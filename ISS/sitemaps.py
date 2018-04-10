1666d351baec7268074681290232fd88721e13c9
RyanJenkins <rjenkins@mail.sfsu.edu>
2018-04-10 11:38:08 -0700

    Removed including each page of a thread in sitemap for perf reasons. Instead we're trying the "pray that google can figure out next/prev link tags on pages pointed to by the sitemap" strategy.

--- ISS/sitemaps.py
+++ ISS/sitemaps.py
@@ -20,49 +20,23 @@ class ForumsSitemap(Sitemap):
         return forum.get_url()
 
 class ThreadSitemap(Sitemap):
-    @staticmethod
-    def _lazy_last_post(thread, n):
-        return lambda: thread.get_posts_in_thread_order()[n]
-
     def items(self):
-        pages = []
-        threads = Thread.objects.filter(forum__is_trash=False)
+        return Thread.objects.filter(forum__is_trash=False)
 
-        for thread in threads:
-            ppp = utils.get_config('posts_per_thread_page')
-            post_count = thread.get_post_count()
-            num_pages = int(math.ceil(post_count / ppp))
-
-            for n in range(1,num_pages+1):
-                url = thread.get_url(page=n)
-                last_page = n == num_pages
-                last_idx = (post_count if last_page else n*ppp) - 1
-                last_post = self._lazy_last_post(thread, last_idx)
-
-                pages.append((thread, url, last_page, last_post))
-
-        return pages
-
-    def changefreq(self, data):
-        thread, url, last_page, last_post = data
-        if last_page:
-            inactive_time = timezone.now() - thread.last_update
-            if  inactive_time < timedelta(days=1):
-                return 'hourly'
-            elif  inactive_time < timedelta(days=28):
-                return 'daily'
-            else:
-                return 'monthly'
+    def changefreq(self, thread):
+        inactive_time = timezone.now() - thread.last_update
+        if  inactive_time < timedelta(days=1):
+            return 'hourly'
+        elif  inactive_time < timedelta(days=28):
+            return 'daily'
         else:
-            return 'never'
+            return 'monthly'
 
-    def lastmod(self, data):
-        thread, url, last_page, last_post = data
-        return last_post().created
+    def lastmod(self, thread):
+        return thread.last_update
     
-    def location(self, data):
-        thread, url, last_page, last_post = data
-        return url
+    def location(self, thread):
+        return thread.get_url()
 
 iss_sitemaps = {
     'forums': ForumsSitemap,
