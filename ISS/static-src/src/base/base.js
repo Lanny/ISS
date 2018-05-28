379a5a8fa4ad85a756f57cf6831b0f3bfeecdb33
RyanJenkins <rjenkins@mail.sfsu.edu>
2018-05-27 23:34:47 -0700

    Fixed issue with best tag in post previews and give base.js a much needed cleanup.

--- ISS/static-src/src/base/base.js
+++ ISS/static-src/src/base/base.js
@@ -1,118 +1,7 @@
 ;(function() {
-  function shakeBaby(baby, intensity) {
-    baby.css({
-      position: 'relative',
-      top: ~~((Math.random() - 0.5) * intensity),
-      left: ~~((Math.random() - 0.5) * intensity)
-    });
 
-    window.setTimeout(shakeBaby.bind(this, baby, intensity), 50);
-  };
-
-  function genCircle(color, size) {
-    return $('<div class="hyper-circle">')
-      .css({
-        background: color,
-        width: size,
-        height: size,
-        'border-radius': size,
-        display: 'block',
-        position: 'fixed'
-      });
-  }
-
-  function startHyperDrive() {
-    var width = $(window).width(),
-      height = $(window).height();
-
-    var circles = [],
-      totalVolume = width * height
-      runningVolume = 0,
-      targetDensity = 5.0;
-
-    while (runningVolume/totalVolume < targetDensity) {
-      var circSize = ~~(100 + Math.random() * (width - 100) / 2),
-        r = ~~(Math.random()*255),
-        g = ~~(Math.random()*255),
-        b = ~~(Math.random()*255),
-        circ = genCircle('rgba('+r+','+g+','+b+',0.3)', circSize);
-
-      circles.push(circ);
-      runningVolume += Math.PI *  circSize * circSize;
-    }
-
-    for (var i=0; i<circles.length; i++) {
-      ;(function() {
-        var aniTarget = circles[i],
-          circSize = aniTarget.width();
-
-        aniTarget.appendTo('body')
-          .css({
-            top: Math.random() * height - circSize / 2,
-            left: Math.random() * width - circSize / 2
-          });
-
-        ;(function animateCircPos() {
-          var targTop = Math.random() * height - circSize / 2,
-            targLeft = Math.random() * width - circSize / 2;
-
-          aniTarget.animate(
-            {top: targTop, left: targLeft}, 
-            {
-              duration: Math.random() * 5000,
-              queue: false,
-              done: animateCircPos
-            });
-        })();
-
-        var period = 1200,
-          amplitude = 0.3,
-          initialDirection = Math.sign(Math.random() - 0.5);
-
-        ;(function animateCircSize() {
-          var sizeOne = circSize * (1 + amplitude * initialDirection),
-            sizeTwo = circSize * (1 - amplitude * initialDirection);
-
-          aniTarget.animate(
-            {width: sizeOne, height: sizeOne},
-            {
-              duration: period / 2,
-              queue: false,
-              done: function() {
-                aniTarget.animate(
-                  {width: sizeTwo, height: sizeTwo},
-                  {
-                    duration: period / 2,
-                    queue: false,
-                    done: animateCircSize
-                  });
-              }
-            });
-        })();
-      })();
-    }
-
-    shakeBaby($('.page-content'), 20);
-
-    var blurPeriod = 2000,
-      degree = 4,
-      t0 = Date.now();
-
-    ;(function blurShit() {
-      var d = (Date.now() - t0) % blurPeriod,
-        s = degree * (d / (blurPeriod * 2));
-
-      if (d > blurPeriod / 2) {
-        s = degree - s;
-      }
-
-      $(document.documentElement).css('filter', 'blur(' + s + 'px)');
-
-      window.setTimeout(blurShit, 1000/24);
-    })();
-  };
-
-  function wrap($, config, bbcode, paginator) {
+  function wrap($, config, bbcode, paginator, hyperDrive) {
+    console.log('HD loaded supposedly');
     function peek() {
       var peekaboo = $('.peekaboo').first()
         .css('display', 'block');
@@ -125,14 +14,13 @@
     }
 
     $(document).on('click', '.hyper-drive', function() {
-      startHyperDrive();
+      hyperDrive.start()
     });
 
     $(function() {
       bbcode.bindRegion($('.page-content'));
       paginator.bindKeyboardControls();
 
-      shakeBaby($('.ex'), 10);
       window.setTimeout(peek, Math.random()*1000*60*60);
 
       if ($('.g-recaptcha').length > 0) {
@@ -146,7 +34,8 @@
     'jquery',
     'config',
     'bbcode',
-    'paginator'
+    'paginator',
+    'hyper-drive'
   ], wrap);
 })();
 
