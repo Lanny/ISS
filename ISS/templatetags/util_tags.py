370798eaac62cbcf65c22366a1d8028e29a73810
Ryan Lanny Jenkins <rjenkins@styleseat.com>
2019-12-16 12:17:16 -0800

    Made post_controls.html render the post_controls inclusion tag so mixed mode renders can use it without messing up the cache black magic implemented previously.

--- ISS/templatetags/util_tags.py
+++ ISS/templatetags/util_tags.py
@@ -12,7 +12,7 @@ from ISS.models import FilterWord, AccessControlList
 
 register = template.Library()
 
-@register.assignment_tag
+@register.simple_tag
 def test_link(test, user):
     if test == 'always':
         return True
@@ -23,10 +23,12 @@ def test_link(test, user):
     elif test == 'is_admin':
         return user.is_authenticated() and user.is_admin
 
+
 @register.simple_tag(name='url_apply')
 def url_apply(pat, args_and_kwargs=None):
     args, kwargs = args_and_kwargs if args_and_kwargs else ((), {})
     return reverse(pat, args=args, kwargs=kwargs)
+
 
 def _get_theme(user):
     if user.is_authenticated:
@@ -36,9 +38,11 @@ def _get_theme(user):
 
     return theme_name
 
+
 @register.simple_tag(name='get_theme')
 def get_theme(user):
     return staticfiles.static("css/%s.css" % _get_theme(user))
+
 
 @register.simple_tag(name='get_theme_color')
 def get_theme_color(user):
@@ -46,6 +50,7 @@ def get_theme_color(user):
     default_theme = utils.get_config('default_theme')
     theme = utils.get_config('themes').get(theme_code, default_theme)
     return theme['color']
+
 
 @register.simple_tag(name='pgp_block')
 def pgp_block(pgp_key, js_enabled=True):
@@ -56,12 +61,15 @@ def pgp_block(pgp_key, js_enabled=True):
 
     return safestring.mark_safe(markup)
 
-@register.inclusion_tag('post_controls.html', takes_context=True)
+
+@register.inclusion_tag(
+    'post_controls_itag.html',
+    takes_context=True)
 def post_controls(context):
     if '_iss_template_cache' not in context:
         context['_iss_template_cache'] = {}
 
-    tcache = context['_iss_template_cache'] 
+    tcache = context['_iss_template_cache']
     user = context['user']
     post = context['post']
 
@@ -74,7 +82,7 @@ def post_controls(context):
     return {
         'user': user,
         'post': context['post'],
-        'user_post_count': tcache['user_post_count'] ,
+        'user_post_count': tcache['user_post_count'],
         'can_edit': post.can_be_edited_by(
             user,
             is_banned=tcache['user_is_banned']
@@ -86,20 +94,24 @@ def post_controls(context):
 def word_filter(value):
     return FilterWord.do_all_replacements(value)
 
+
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
 
 @register.filter
 def check_acl(poster, acl_name):
     acl = AccessControlList.get_acl(acl_name)
     return acl.is_poster_authorized(poster)
 
+
 @register.filter(expects_localtime=True, is_safe=False)
 def present_dt(dt):
     return '%s at %s' % (
         defaultfilters.date(dt, 'Y-m-d'),
         defaultfilters.time(dt, 'f A e'))
+
 
 @register.filter(expects_localtime=True, is_safe=False)
 def present_td(td):
