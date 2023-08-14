03bfb788e04e9fc17790de68cd284e3542c66ea0
lanny <lan.rogers.book@gmail.com>
2023-08-14 12:31:54 -0500

    All providing for a external settings file.

--- manage.py
+++ manage.py
@@ -3,7 +3,16 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    settings_file_path = os.environ.get("ISS_SETTINGS_FILE")
+
+    if not settings_file_path:
+        raise Exception("Must provide a `ISS_SETTINGS_FILE` env var")
+
+    settings_dir, settings_file = os.path.split(settings_file_path)
+    settings_mod_name = os.path.splitext(settings_file)[0]
+
+    sys.path.append(os.path.abspath(settings_dir))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_mod_name)
 
     from django.core.management import execute_from_command_line
 
