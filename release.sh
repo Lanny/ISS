f57aa968517102d937e2e3c50c704236aba28876
lanny <lan.rogers.book@gmail.com>
2025-02-28 20:15:16 -0600

    Add release script, changelog, and version file

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+MAJOR=$(cat VERSION | sed -r 's/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/')
+MINOR=$(cat VERSION | sed -r 's/([0-9]+)\.([0-9]+)\.([0-9]+)/\1.\2/')
+FULL=$(head -n 1 VERSION)
+
+
+git diff-files --quiet
+if [ $? -eq 1 ]; then
+  echo "Uncomitted changes in git, bailing"
+  exit 1
+fi
+
+
+git tag "v$FULL"
+docker build \
+  -t lannysport/iss:latest \
+  -t lannysport/iss:$MAJOR \
+  -t lannysport/iss:$MINOR \
+  -t lannysport/iss:$FULL \
+  .
+
+docker push lannysport/iss:latest
+docker push lannysport/iss:$MAJOR
+docker push lannysport/iss:$MINOR
+docker push lannysport/iss:$FULL
