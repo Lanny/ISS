f479b7d84d3e32003a8c563b24898aeea906dd82
lanny <lan.rogers.book@gmail.com>
2025-06-10 11:34:48 -0500

    Add the `tstm` function to the admin shell

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 ### Features
 - Added as "mark all as read" action in the private messages list views
 - Blacklist a few non-canonical URLs that contain post content (e.g. user list-of-posts pages, post-by-id URLs) in robots.txt
+- Add the `tstm` function to the admin shell so I don't have to go look up the source listing every time I do this
 
 ## 1.1.0
 
--- src/ISS/shell_startup.py
+++ src/ISS/shell_startup.py
@@ -2,5 +2,14 @@ from django.utils import timezone
 from datetime import datetime, timedelta
 from ISS.models import *
 
+def tstm(poster):
+    junk_user = Poster.get_or_create_junk_user()
+    poster.merge_into(junk_user)
+    poster.is_active = False
+    poster.save()
+
+    print('Successfully anonymized user with id: %d' % poster.pk)
+
 print('Django\'s `timezone`, datetimes\'s `datetime` and `timedelta`, and '
-      'all of the ISS models have been imported and are currently in scope')
+      'all of the ISS models have been imported and are currently in scope. '
+      'The tstm() function has also been made available.')
